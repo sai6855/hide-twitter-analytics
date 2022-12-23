5cd7d6c792300476cbc4116999d61fdaee2dfe9e
sai6855 <60743144+sai6855@users.noreply.github.com>
2022-12-23 11:28:05 +0530

    readme

--- contentScript.js
+++ contentScript.js
@@ -1,9 +1,10 @@
 window.addEventListener("scroll", () => {
   const links = document.getElementsByTagName("a");
   for (let i = 0; i < links.length; i++) {
-    if (links[i].href.includes("analytics")) {
-      console.log(links[i].href);
-      links[i].style.display = "none";
-    }
+    if (window.location.href.includes("twitter.com"))
+      if (links[i].href.includes("analytics")) {
+        console.log(links[i].href);
+        links[i].style.display = "none";
+      }
   }
 });
