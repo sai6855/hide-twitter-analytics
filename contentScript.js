05472f1a898a7f60be3912514e6293dce371cf7e
sai6855 <60743144+sai6855@users.noreply.github.com>
2022-12-23 11:36:44 +0530

    removed log

--- contentScript.js
+++ contentScript.js
@@ -3,7 +3,6 @@ window.addEventListener("scroll", () => {
   for (let i = 0; i < links.length; i++) {
     if (window.location.href.includes("twitter.com"))
       if (links[i].href.includes("analytics")) {
-        console.log(links[i].href);
         links[i].style.display = "none";
       }
   }
